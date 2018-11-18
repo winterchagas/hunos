7a0e5f43ff1fb1e2657b9eefad5025e6fb9d2f57
winterchagas <winterchagas@gmail.com>
2018-11-17 23:45:49 -0800

    added questions

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,52 +1,102 @@
 export default [
   {
-    id: 1234,
-    question: '1 What are the four important attributes that all professional software should have?',
+    id: 1230,
+    question: 'Who invented surfing?',
     answers: [
-      {letter: 'A', answer: 'maintainability, dependability, performance, usability'},
-      {letter: 'B', answer: 'maintainability, dependability, performance, usability'},
-      {letter: 'C', answer: 'maintainability, dependability, performance, usability'},
-      {letter: 'D', answer: 'maintainability, dependability, performance, usability'}
-    ]
-  },
-  {
-    id: 1233,
-    question: 'QUESTION 2?',
-    answers: [
-      {letter: 'A', answer: 'yoooo'},
-      {letter: 'B', answer: 'yeyyyy'},
-      {letter: 'C', answer: 'haaaaa'},
-      {letter: 'D', answer: 'huuuuummmm'}
-    ]
-  },
-  {
-    id: 1232,
-    question: '3 What are the four important attributes that all professional software should have?',
-    answers: [
-      {letter: 'A', answer: 'maintainability, dependability, performance, usability'},
-      {letter: 'B', answer: 'maintainability, dependability, performance, usability'},
-      {letter: 'C', answer: 'maintainability, dependability, performance, usability'},
-      {letter: 'D', answer: 'maintainability, dependability, performance, usability'}
+      {letter: 'A', answer: 'Australian Aboriginals'},
+      {letter: 'B', answer: 'Spanish Colonists'},
+      {letter: 'C', answer: 'Ancient Greeks'},
+      {letter: 'D', answer: 'Polynesians'}
     ]
   },
   {
     id: 1231,
-    question: '4 What are the four important attributes that all professional software should have?',
+    question: 'Where can you find the famous "Pipeline" surfing location?',
     answers: [
-      {letter: 'A', answer: 'maintainability, dependability, performance, usability'},
-      {letter: 'B', answer: 'maintainability, dependability, performance, usability'},
-      {letter: 'C', answer: 'maintainability, dependability, performance, usability'},
-      {letter: 'D', answer: 'maintainability, dependability, performance, usability'}
+      {letter: 'A', answer: 'California'},
+      {letter: 'B', answer: 'Portugal'},
+      {letter: 'C', answer: 'South Africa'},
+      {letter: 'D', answer: 'Hawaii'}
+    ]
+  },
+  {
+    id: 1232,
+    question: 'A surfer that rides with his right foot forward is called ____.',
+    answers: [
+      {letter: 'A', answer: 'Goofy'},
+      {letter: 'B', answer: 'Southpaw'},
+      {letter: 'C', answer: 'Funky'},
+      {letter: 'D', answer: 'Weird'}
+    ]
+  },
+  {
+    id: 1233,
+    question: 'What is it called when a surfer wraps five toes over the front of the board?',
+    answers: [
+      {letter: 'A', answer: 'Wipeout'},
+      {letter: 'B', answer: 'Nose grab'},
+      {letter: 'C', answer: 'Nollie'},
+      {letter: 'D', answer: 'Hanging Five'}
+    ]
+  },
+  {
+    id: 1234,
+    question: 'What is a "wipeout"?',
+    answers: [
+      {letter: 'A', answer: 'A trick where you stand on your hands'},
+      {letter: 'B', answer: 'Falling off the board'},
+      {letter: 'C', answer: 'A quick turn on top of the wave'},
+      {letter: 'D', answer: 'A series of difficult waves'}
     ]
   },
   {
     id: 1235,
-    question: '5 What are the four important attributes that all professional software should have?',
+    question: 'What is the record for most people riding a single surfboard?',
     answers: [
-      {letter: 'A', answer: 'maintainability, dependability, performance, usability'},
-      {letter: 'B', answer: 'maintainability, dependability, performance, usability'},
-      {letter: 'C', answer: 'maintainability, dependability, performance, usability'},
-      {letter: 'D', answer: 'maintainability, dependability, performance, usability'}
+      {letter: 'A', answer: '3'},
+      {letter: 'B', answer: '12'},
+      {letter: 'C', answer: '26'},
+      {letter: 'D', answer: '66'}
+    ]
+  },
+  {
+    id: 1236,
+    question: 'What professional surfer has been crowned World Surf League Champion a record 11 times?',
+    answers: [
+      {letter: 'A', answer: 'Layne Beachley'},
+      {letter: 'B', answer: 'Mick Fanning'},
+      {letter: 'C', answer: 'Kelly Slater'},
+      {letter: 'D', answer: 'Mark Richards'}
+    ]
+  },
+  {
+    id: 1237,
+    question: 'Mick Fanning is a champion of the World Surfing League. But what is he most famous for?',
+    answers: [
+      {letter: 'A', answer: 'Surfing blindfolded'},
+      {letter: 'B', answer: 'Inventing many tricks'},
+      {letter: 'C', answer: 'Surfing backwards'},
+      {letter: 'D', answer: 'Punching a shark'}
+    ]
+  },
+  {
+    id: 1238,
+    question: 'What is the world record for personal ownership of surfboards?',
+    answers: [
+      {letter: 'A', answer: '647'},
+      {letter: 'B', answer: '1098'},
+      {letter: 'C', answer: '85'},
+      {letter: 'D', answer: '212'}
+    ]
+  },
+  {
+    id: 1239,
+    question: 'The International Surfing Day',
+    answers: [
+      {letter: 'A', answer: 'August 3'},
+      {letter: 'B', answer: 'February 28'},
+      {letter: 'C', answer: 'December 21'},
+      {letter: 'D', answer: 'June 20'}
     ]
   }
 ]
